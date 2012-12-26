5304d9a1c28a6a5820ac212c6fe6572a48296eca
YLFmagina <ylfmagina@gmail.com>
2012-12-26 20:00:27 +0800

    com depot

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '56b99232fa9d0b0198600494b2f9256fe8a9f787e2a67549dc263eb8b45b2b322528551377b7c4ef114cd7b8275d54821c2e56f0279b4cb36a637a18530e456c'
